ce6cbcaab45dfb81d86c5ceb30ce84ffaa8712a8
Rohan Peshwe <37624870+rohanapeshwe@users.noreply.github.com>
2022-05-26 13:17:25 +0530

    refactor(item component): seperated the props and added styling

--- src/components/item/index.tsx
+++ src/components/item/index.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
 
-export type onHandleChangeProps = {
+export type onHandleChangeArgsProps = {
   checked: boolean
   text: string
+}
+
+export type onHandleChangeProps = {
+  onHandleChange: (props: onHandleChangeArgsProps) => void
 }
 
 export type ItemProps = {
   text: string
   isChecked: boolean
-  onHandleChange: (props: onHandleChangeProps) => void
 }
 
-const Item = ({ text, isChecked, onHandleChange }: ItemProps) => {
+type ListItemComponentProps = ItemProps & onHandleChangeProps
+
+const ListItem = ({ text, isChecked, onHandleChange }: ListItemComponentProps) => {
   return (
     <li
-      className="inline-flex items-center justify-start w-64 p-2 list-none border-2 bg-stone-300 border-stone-500"
+      className="inline-flex items-center justify-start p-2 my-1 list-none border-2 bg-stone-300 border-stone-500"
       role="listitem"
     >
       <input
@@ -29,17 +34,17 @@ const Item = ({ text, isChecked, onHandleChange }: ItemProps) => {
           })
         }}
       />
-      <label className="ml-2 truncate" htmlFor={`${text}-id`}>
+      <label className={`ml-2 truncate ${isChecked && 'line-through'}`} htmlFor={`${text}-id`}>
         {text}
       </label>
     </li>
   )
 }
 
-Item.defaultProps = {
+ListItem.defaultProps = {
   text: 'No text added',
   isChecked: false,
   onHandleChange: () => {},
 }
 
-export default Item
+export default ListItem
