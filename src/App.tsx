fce785cb4cf6bda13163727cc98ba25e3e557682
Rohan Peshwe <37624870+rohanapeshwe@users.noreply.github.com>
2022-05-26 13:19:03 +0530

    refactor(app): added list and store to list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import Item from './components/item'
+import { useReducer } from 'react'
+import List from './components/list'
+import { actions, initialState, reducer } from './reducers/store'
+import { onHandleChangeArgsProps } from './components/item'
+import Input from './components/input'
 
 function App() {
-  const title =
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, facilis magnam nulla labore molestiae esse officia dolorum veritatis cumque culpa fuga tempore! Sunt ex ipsa ab, iste in reiciendis natus.'
-  return <Item text={title} />
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const handleChange = ({ checked, text }: onHandleChangeArgsProps) => {
+    dispatch({
+      type: actions.ITEM_UPDATE,
+      payload: {
+        isChecked: checked,
+        text,
+      },
+    })
+  }
+
+  const handleAdd = (text: string) => {
+    dispatch({
+      type: actions.ITEM_ADD,
+      payload: {
+        isChecked: false,
+        text,
+      },
+    })
+  }
+
+  const handleRemove = ({ checked, text }: onHandleChangeArgsProps) => {
+    dispatch({
+      type: actions.ITEM_REMOVE,
+      payload: {
+        isChecked: checked,
+        text,
+      },
+    })
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full p-2 bg-stone-300">
+      <h1 className="mb-4 font-sans text-3xl font-extrabold text-stone-700">Todos App</h1>
+      <div className="w-72">
+        <Input onEnter={handleAdd} />
+        <List items={state.items} onHandleChange={handleChange} onHandleRemove={handleRemove} />
+      </div>
+    </div>
+  )
 }
 
 export default App
